2317204d92a435153a792128b45aa79aff6dd4ca
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-05-08 18:01:37 +0200

    new

--- src/lesson_5/HomeWork/Car.java
+++ src/lesson_5/HomeWork/Car.java
@@ -4,11 +4,13 @@ public class Car extends Vehicle{
     public Car(String plateNumber, String manufacturer, int year, String color) {
         super(plateNumber, manufacturer, year, color);
     }
-
     @Override
     public String toString() {
-        return "Car{" +
-                "isMoving=" + isMoving +
+        return "Truck{" +
+                "Производитель='" + getManufacturer() + '\'' +
+                ", Год выпуска=" + getYear() +
+                ", Цвет='" + getColor() + '\'' +
+                ", Номера ='" + getPlateNumber() + '\'' +
                 '}';
     }
 }
--- src/lesson_5/HomeWork/VehicleApp.java
+++ src/lesson_5/HomeWork/VehicleApp.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class VehicleApp {
     public static void main(String[] args) {
         List<Vehicle> vehicles = new ArrayList<>();
-//
-//        vehicles.add(new Bus("32452", "VW", 8, 2024,"black"));
-//        vehicles.add(new Truck("234", "MAN", 12_000, 2010, "red"));
-//        System.out.println(vehicles);
+
+        vehicles.add(new Bus("32452", "VW", 8, 2024,"black"));
+        vehicles.add(new Truck("234", "MAN", 12_000, 2010, "red"));
+        System.out.println(vehicles);
 
         Bus bus1 = new Bus("23421","VW", 8,2024, "white");
         Truck truck = new Truck("234", "MAN", 12_000, 2010, "red");
