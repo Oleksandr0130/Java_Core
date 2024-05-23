ce534b1abcc682fb2f5bdf2662fe6e5385b3ea7b
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-05-23 12:47:13 +0200

    new

--- src/lesson_14/HomeWork/Task2/StudentApp.java
+++ src/lesson_14/HomeWork/Task2/StudentApp.java
@@ -2,16 +2,35 @@ package lesson_14.HomeWork.Task2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class StudentApp {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>(Arrays.asList(
-                new Student("Oleg", Arrays.asList(5,4,6,7,8,25,16)),
-                new Student("Aleksandr", Arrays.asList(3,6,6,1,10,40,26)),
-                new Student("Oleg", Arrays.asList(5,4,0,3,18,15,36)),
-                new Student("Oleg", Arrays.asList(5,40,6,7,8,25))
+                new Student("Oleg", Arrays.asList(5, 4, 6, 7, 8, 10, 10, 2, 3, 10)),
+                new Student("Aleksandr", Arrays.asList(3, 6, 6, 1, 10, 10, 6, 4, 2, 10)),
+                new Student("Katia", Arrays.asList(5, 4, 0, 3, 10, 10, 1, 8, 10, 10)),
+                new Student("Andrey", Arrays.asList(5, 10, 6, 7, 8, 10, 3, 5, 6, 9))
         ));
-        System.out.println(students.toString());
+        System.out.println(grades(students));
+    }
+
+    public static String grades(List<Student> students) {
+        Collections.sort(students, (student1, student2) -> Integer.compare(student1.getMissedGrades(), student2.getMissedGrades()));
+
+        StringBuilder sb = new StringBuilder();
+        for (Student student : students) {
+            sb.append(student.getName())
+                    .append(" - ")
+                    .append(student.getMissedGrades())
+                    .append(";");
+        }
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1);
+        }
+
+        return sb.toString();
     }
 }
+
